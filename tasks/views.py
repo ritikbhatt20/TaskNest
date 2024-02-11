f073a14c7fed5332dd79ca4c2a7c5c4d5fdeb13d
ritik <ritikbhatt020@gmail.com>
2024-02-11 13:04:00 +0530

    🔨Created form using ModelForm

--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from .forms import *
 
 # Create your views here.
 def index(request):
     # return HttpResponse("Hello world")
     tasks = Task.objects.all()
-    context = { 'tasks': tasks }
+    
+    form = TaskForm()
+    context = { 'tasks': tasks, 'form': form }
     return render(request, 'tasks/list.html', context)
